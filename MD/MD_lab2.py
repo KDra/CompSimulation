f3f00dd08ad2c349faab2ed8a5d17ebe7a258196
Kostas Dra <kostas@Kostass-MacBook-Air.local>
2015-10-31 13:12:33 +0000

    insignificant update

--- MD/MD_lab2.py
+++ MD/MD_lab2.py
@@ -15,3 +15,12 @@ rcParams['font.size'] = 16
 rcParams['figure.figsize'] = (12,6)
 
 
+def harmonic(r):
+    return 0.5 * 2743.0 * (r - 1.1283)**2
+
+def kretzer(r):
+    return 258.9 * ((r - 1.1283)/float(r))**2
+
+def morse(r):
+    return 258.9 * (1 - np.exp(-2.302 * (r - 1.1283)))**2
+
